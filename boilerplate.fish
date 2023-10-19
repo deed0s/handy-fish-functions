4de2ff3fa80d00aff30265c988b0b2877614bbdb
deedos <devkieran@proton.me>
2023-10-19 19:53:20 +0100

    fixed app.js and main.css file creation

--- boilerplate.fish
+++ boilerplate.fish
@@ -1,7 +1,7 @@
 function boilerplate -d "Create a static html boilerplate project"
     mkdir -p $argv/style $argv/src $argv/img
     cp ~/templates/index.html $argv/
-    touch app.js $argv/src/
-    touch main.css $argv/style/
+    touch app.js $argv/src/app.js
+    touch main.css $argv/style/main.css
     cd $argv
 end
