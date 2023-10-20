d116daca2677584a00aaffd2362c40112c237d94
Kieran aka deedos <devkieran@proton.me>
2023-10-20 23:54:14 +0100

    refactored function to allow user to define template directory

--- boilerplate.fish
+++ boilerplate.fish
@@ -1,7 +1,15 @@
 function boilerplate -d "Create a static html boilerplate project"
-    mkdir -p $argv/style $argv/src $argv/img
-    cp ~/templates/index.html $argv/
-    touch app.js $argv/src/app.js
-    touch main.css $argv/style/main.css
-    cd $argv
+    argparse h/help -- $argv
+    or return
+
+    if set -ql _flag_help
+        echo "Usage: boilerplate <project name> <path to html template>"
+        return 0
+    end
+
+    mkdir -p $argv[1]/style $argv[1]/src $argv[1]/img
+    cp $argv[2]index.html $argv[1]/
+    touch app.js $argv[1]/src/app.js
+    touch main.css $argv[1]/style/main.css
+    cd $argv[1]
 end
